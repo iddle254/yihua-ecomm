575dcfb6e75b74a5e07451a1da906ebbb3a6ac7f
Moss <charlesiddle99@gmail.com>
2021-07-26 00:49:17 +0300

    homepage styled

---
+++ src/components/directory/directory-component.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory-styles.scss";
+
+export default class DirectoryComponent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl:
+            "https://images.unsplash.com/photo-1562929154-3525cdc73fa5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+          id: 1,
+          linkUrl: "shop/hats",
+        },
+        {
+          title: "jackets",
+          imageUrl:
+            "https://images.unsplash.com/photo-1555991637-c403f0b02997?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fGphY2tldHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          id: 2,
+          linkUrl: "shop/jackets",
+        },
+        {
+          title: "sneakers",
+          imageUrl:
+            "https://images.unsplash.com/photo-1612902376491-7a8a99b424e8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          id: 3,
+          linkUrl: "shop/sneakers",
+        },
+        {
+          title: "womens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1492467008961-6e33db522c9b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNob3BpZnklMjBkcmVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+        },
+        {
+          title: "mens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1543807535-eceef0bc6599?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE2fHxzaG9waWZ5JTIwbWVufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, id, imageUrl, size, linkUrl }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
