38fed325ea099350a6f95a2a266a39496c086b92
Moss <charlesiddle99@gmail.com>
2021-08-01 14:58:17 +0300

    created and styled the signup and signin page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { React, Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shoppage/shoppage.component";
+import Header from "./components/header/header.component";
+import SigninSignup from "./pages/signin-&-signup/signin&signup.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscibeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscibeFromAuth();
+  }
+  render() {
+    return (
+      <>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SigninSignup} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/signin-&-signup/signin&signup.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Signin from "../../components/signin/signin.component";
+
+function SigninSignup() {
+  return (
+    <div className="signin-and-signup">
+      <h1>Signin</h1>
+      <Signin />
+    </div>
+  );
+}
+
+export default SigninSignup;
